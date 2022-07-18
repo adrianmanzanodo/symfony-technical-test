f3b0dc745bbfdaae006df8cbeec8bc423389ee2f
Adrian Manzano <adrian.manzao.do@gmail.com>
2022-07-18 19:21:36 +0200

    Configure crud actions

---
+++ src/Controller/Admin/PeliculasCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Peliculas;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
+class PeliculasCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Peliculas::class;
+    }
+
+
+    public function configureActions(Actions $actions): Actions
+{
+    return $actions
+        // ...
+        ->remove(Crud::PAGE_INDEX, Action::NEW)
+        ->remove(Crud::PAGE_DETAIL, Action::EDIT)
+        ->remove(Crud::PAGE_DETAIL, Action::DELETE)
+    ;
+}
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
